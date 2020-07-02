0bed0d0720dd681dc1d24ce545144b935a2199e7
PHY <PHY@DESKTOP-71DBNFQ>
2020-07-02 22:14:39 +0900

    페이징 기능

--- board/src/main/java/com/board/controller/BoardController.java
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.board.dao.BoardDAO;
 import com.board.domain.BoardVO;
+import com.board.domain.Page;
 import com.board.service.BoardService;
 
 @Controller
@@ -81,19 +82,41 @@ public class BoardController {
 	// 게시물 목록 + 페이징 추가
 	@RequestMapping(value = "/listPage", method = RequestMethod.GET)
 	public void getListPage(Model model, @RequestParam("num") int num) throws Exception {
-	 
+		
+		Page page = new Page();
+		
+		page.setNum(num);
+		page.setCount(service.count());  
+
+		List list = null; 
+		list = service.listPage(page.getDisplayPost(), page.getPostNum());
+
+		model.addAttribute("list", list);   
+		/*
+		model.addAttribute("pageNum", page.getPageNum());
+
+		model.addAttribute("startPageNum", page.getStartPageNum());
+		model.addAttribute("endPageNum", page.getEndPageNum());
+
+		  model.addAttribute("prev", page.getPrev());
+		model.addAttribute("next", page.getNext());  
+		 */
+		model.addAttribute("page", page);
+		model.addAttribute("select", num);
+		
+		/*
 	 // 게시물 총 갯수
 	 int count = service.count();
-	  
+
 	 // 한 페이지에 출력할 게시물 갯수
 	 int postNum = 10;
-	  
+
 	 // 하단 페이징 번호 ([ 게시물 총 갯수 ÷ 한 페이지에 출력할 갯수 ]의 올림)
 	 int pageNum = (int)Math.ceil((double)count/postNum);
-	  
+
 	 // 출력할 게시물
 	 int displayPost = (num - 1) * postNum;
-	 
+
 	// 한번에 표시할 페이징 번호의 갯수
 	 int pageNum_cnt = 10;
 
@@ -102,22 +125,22 @@ public class BoardController {
 
 	 // 표시되는 페이지 번호 중 첫번째 번호
 	 int startPageNum = endPageNum - (pageNum_cnt - 1);
-	 
+
 	// 마지막 번호 재계산
 	 int endPageNum_tmp = (int)(Math.ceil((double)count / (double)pageNum_cnt));
-	  
+
 	 if(endPageNum > endPageNum_tmp) {
 	  endPageNum = endPageNum_tmp;
 	 }
-	 
+
 	 boolean prev = startPageNum == 1 ? false : true;
 	 boolean next = endPageNum * pageNum_cnt >= count ? false : true;
-	    
+
 	 List list = null; 
 	 list = service.listPage(displayPost, postNum);
 	 model.addAttribute("list", list);   
 	 model.addAttribute("pageNum", pageNum);
-	 
+
 	// 시작 및 끝 번호
 	 model.addAttribute("startPageNum", startPageNum);
 	 model.addAttribute("endPageNum", endPageNum);
@@ -125,9 +148,10 @@ public class BoardController {
 	 // 이전 및 다음 
 	 model.addAttribute("prev", prev);
 	 model.addAttribute("next", next);
-	 
+
 	// 현재 페이지
 	 model.addAttribute("select", num);
+		 */
 	}
 
 
